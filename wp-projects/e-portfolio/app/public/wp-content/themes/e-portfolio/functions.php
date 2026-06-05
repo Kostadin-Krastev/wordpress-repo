50e8b8c7f1fc5429e60eff442b27e59288b0005f
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-06-05 22:11:22 +0300

    build the header

---
+++ wp-projects/e-portfolio/app/public/wp-content/themes/e-portfolio/functions.php
@@ -0,0 +1,25 @@
+<?php
+function load_stylesheets() {
+    wp_register_style("bootstrapcss", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", array(), 1, "all"); // "all" means that it will apply to every device. It is "all" by default so it is unnecessary to be written
+    wp_enqueue_style("bootstrapcss");
+
+    wp_register_style("googlefont", "https://fonts.googleapis.com/css?family=Lato", array(), 1, "all");
+    wp_enqueue_style("googlefont");
+
+    wp_register_style("stylecss", get_theme_file_uri("css/style.css"), array(), 1);
+    wp_enqueue_style("stylecss");
+
+    wp_register_style("stylecss2", get_theme_file_uri("css/style2.css"), array(), 1);
+    wp_enqueue_style("stylecss2");
+}
+
+add_action("wp_enqueue_scripts", "load_stylesheets");
+
+function load_scripts() {
+    wp_enqueue_script("jquery"); // Use WordPress's built-in jQuery
+
+    wp_register_script("bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js", array("jquery"), 1, true); //true if its going to be printed in the footer. false if not.
+    wp_enqueue_script("bootstrap");
+}
+
+add_action("wp_enqueue_scripts", "load_scripts");
---
+++ wp-projects/e-portfolio/app/public/wp-content/themes/e-portfolio/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" type="image/jpg" href="images/portrait.jpg" />
+      <title>E-Portfolio</title>
+      <?php wp_head(); ?>
+   </head>
+
+   <body data-spy="scroll" data-target=".navbar" data.offset="60">
+      <nav class="navbar navbar-default navbar-fixed-top">
+         <div class="container">
+            <div class="navbar-header">
+               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#monMenu">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+               </button>
+            </div>
+
+            <div class="collapse navbar-collapse" id="monMenu">
+               <ul class="nav navbar-nav">
+                  <li><a href="#about">Me</a></li>
+                  <li><a href="#skills">Skills</a></li>
+                  <li><a href="#internships">Internships</a></li>
+                  <li><a href="#experience">Experiences</a></li>
+                  <li><a href="#education">Education</a></li>
+                  <li><a href="#portfolio">Portfolio</a></li>
+               </ul>
+            </div>
+         </div>
+      </nav>
