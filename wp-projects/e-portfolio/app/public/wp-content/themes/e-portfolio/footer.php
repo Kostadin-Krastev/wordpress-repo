f17035aca75b5fcb762dad45b842ef0f04c9ef57
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-06-10 22:50:27 +0300

    replacing static color with  dynamic

--- wp-projects/e-portfolio/app/public/wp-content/themes/e-portfolio/footer.php
+++ wp-projects/e-portfolio/app/public/wp-content/themes/e-portfolio/footer.php
@@ -1,8 +1,11 @@
  <footer class="text-center">
+   <?php $theme_color = get_field('theme_color'); ?>
          <a href="#about">
             <span class="glyphicon glyphicon-chevron-up"></span>
          </a>
-         <h5>© Online CV 2024<br /><br />Developped by <font color="#d82c2e">ME</font></h5>
+         <h5>© Online CV 2024<br /><br />Developped by <font color="<?php 
+         if($theme_color) : echo $theme_color; ?>">ME</font></h5>
+         <?php endif; ?>
       </footer>
       <?php
       wp_footer(); 
