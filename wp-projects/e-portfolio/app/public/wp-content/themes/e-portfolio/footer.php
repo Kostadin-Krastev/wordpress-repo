3e965e570958d8267c54c8c7bbd07d605d6e5c84
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-06-10 21:03:02 +0300

    creat footer

---
+++ wp-projects/e-portfolio/app/public/wp-content/themes/e-portfolio/footer.php
@@ -0,0 +1,11 @@
+ <footer class="text-center">
+         <a href="#about">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+         </a>
+         <h5>© Online CV 2024<br /><br />Developped by <font color="#d82c2e">ME</font></h5>
+      </footer>
+      <?php
+      wp_footer(); 
+       ?>
+   </body>
+</html>
