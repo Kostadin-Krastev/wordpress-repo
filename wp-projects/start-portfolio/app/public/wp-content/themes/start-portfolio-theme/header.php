3de673ec6a71ae2e1aa3bd0dbe823d103df34507
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-07-02 12:12:08 +0300

    add header html

---
+++ wp-projects/start-portfolio/app/public/wp-content/themes/start-portfolio-theme/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header class="header-section">
+        <div class="header-section-container">
+            <div class="logo-container">
+                <img src="images/logo-start.svg" alt="logo of the website Start">
+                <strong class="sub-heading logo-name">Start</strong>
+            </div>
+            <div class="burger-menu open-burger-menu"></div>
+            <nav class="main-menu-container">
+                <ul class="main-menu-items">
+                    <li class="main-menu-item-home"><a class="home-menu-link" href="./index.html">Home</a></li>
+                    <li class="main-menu-item-portfolio"><a class="portfolio-menu-link"
+                            href="./portfolio.html">Portfolio</a>
+                    </li>
+                    <li class="main-menu-item-services"><a class="services-menu-link sub-heading"
+                            href="./services.html">Services</a>
+                    </li>
+                    <li class="main-menu-item-contact"><a class="contact-menu-link sub-heading"
+                            href="./contact-us.html">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
